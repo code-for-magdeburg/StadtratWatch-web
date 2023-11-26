1882a7312ce031bfbc0ddc24cf31d18ab8c1228b
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2023-11-26 20:18:49 +0100

    add application success rate chart

--- src/app/fractions/fractions.component.ts
+++ src/app/fractions/fractions.component.ts
@@ -2,6 +2,7 @@ import { Component, QueryList, ViewChildren } from '@angular/core';
 import { FractionDto } from '../model/Fraction';
 import { FractionsService } from '../services/fractions.service';
 import { compare, SortableFractionsDirective, SortFractionsEvent } from './sortable-fractions.directive';
+import { ChartConfiguration } from 'chart.js';
 
 
 @Component({
@@ -17,6 +18,8 @@ export class FractionsComponent {
   public fractions: FractionDto[] = [];
   public sortedFractions: FractionDto[] = [];
 
+  public applicationSuccessRateChartData: ChartConfiguration<'bar'>['data'] | undefined = undefined;
+  public applicationSuccessRateChartOptions: ChartConfiguration<'bar'>['options'] | undefined = undefined;
 
   @ViewChildren(SortableFractionsDirective) headers: QueryList<SortableFractionsDirective> | undefined;
 
@@ -27,12 +30,16 @@ export class FractionsComponent {
 
   //noinspection JSUnusedGlobalSymbols
   ngOnInit() {
+
     this.fractionsService
       .fetchFractions()
       .subscribe(fractions => {
         this.fractions = this.sortedFractions = this.data = fractions;
         this.sortedFractions.sort((a, b) => b.membersCount - a.membersCount);
+
+        this.setUpApplicationSuccessRateChart(fractions);
       });
+
   }
 
 
@@ -58,6 +65,37 @@ export class FractionsComponent {
         return sortEvent.direction === 'asc' ? res : -res;
       });
     }
+
+  }
+
+
+  private setUpApplicationSuccessRateChart(fractions: FractionDto[]) {
+
+    const applicationsSuccessRateData = fractions
+      .map(f => ({
+        fraction: f.name,
+        value: f.applicationsSuccessRate * 100
+      }))
+      .sort((a, b) => b.value - a.value);
+
+    this.applicationSuccessRateChartData = {
+      labels: applicationsSuccessRateData.map(
+        fraction => `${fraction.fraction} ${fraction.value.toFixed(1)}%`
+      ),
+      datasets: [{ data: applicationsSuccessRateData.map(fraction => fraction.value) }]
+    };
+
+    this.applicationSuccessRateChartOptions = {
+      responsive: true,
+      indexAxis: 'y',
+      scales: {
+        x: { display: false, max: 100, ticks: { display: false } },
+        y: {
+          grid: { display: false, drawTicks: false, drawBorder: false },
+          ticks: { mirror: true, color: 'black', font: { size: 14 }, z: 1 }
+        }
+      }
+    };
 
   }
 
