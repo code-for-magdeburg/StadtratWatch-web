ec1c727f1c1414cf01e0fdd92f8e936401d1188f
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2024-03-25 20:42:32 +0100

    add initial speakers data files

--- src/app/components/votings-success-rate-chart/votings-success-rate-chart.component.ts
+++ src/app/components/votings-success-rate-chart/votings-success-rate-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
 import { FractionLightDto } from '../../model/Fraction';
 import { PartyDto } from '../../model/Party';
@@ -25,7 +25,7 @@ export class VotingsSuccessRateChartData {
   templateUrl: './votings-success-rate-chart.component.html',
   styleUrls: ['./votings-success-rate-chart.component.scss']
 })
-export class VotingsSuccessRateChartComponent {
+export class VotingsSuccessRateChartComponent implements OnChanges {
 
 
   public votingsSuccessRateChartData: ChartConfiguration<'bar'>['data'] | undefined = undefined;
@@ -35,7 +35,6 @@ export class VotingsSuccessRateChartComponent {
   @Input() data: VotingsSuccessRateChartData[] = [];
 
 
-  //noinspection JSUnusedGlobalSymbols
   ngOnChanges(changes: SimpleChanges) {
     if (changes['data']) {
       this.setUpVotingsSuccessRateChart(changes['data'].currentValue);
