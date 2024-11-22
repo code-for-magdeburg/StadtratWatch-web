4f3fbc3e2594dc039a440174da2a6e3ad04b9e12
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2024-11-22 19:57:39 +0100

    convert generate-routes-file to deno

---
+++ src/scripts/generate-routes-file/cli.ts
@@ -0,0 +1,46 @@
+import { parseArgs as stdCliParseArgs } from '@std/cli/parse-args';
+
+
+export type GenerateRoutesFileArgs = {
+  help: boolean;
+  dataDir: string;
+  outputDir: string;
+};
+
+
+export function parseArgs(args: string[]): GenerateRoutesFileArgs {
+  return stdCliParseArgs(args, {
+    boolean: ['help'],
+    string: ['data-dir', 'output-dir'],
+    alias: {
+      help: 'h',
+      'data-dir': ['d', 'dataDir'],
+      'output-dir': ['o', 'outputDir']
+    },
+  }) as GenerateRoutesFileArgs;
+}
+
+
+export function checkArgs(args: GenerateRoutesFileArgs) {
+  const { dataDir, outputDir } = args;
+
+  if (!dataDir) {
+    console.error('Missing data directory. See --help for usage.');
+    Deno.exit(1);
+  }
+
+  if (!outputDir) {
+    console.error('Missing output directory. See --help for usage.');
+    Deno.exit(1);
+  }
+}
+
+
+export function printHelpText() {
+  console.log(`
+Usage: deno run index.ts -i <input-dir> -o <output-dir> -s <scraped-session-file>
+-h, --help                  Show this help message and exit.
+-d, --data-dir              The data directory. It must contain the registry.json file.
+-o, --output-dir            The output directory.
+  `);
+}
--- src/scripts/generate-routes-file/index.ts
+++ src/scripts/generate-routes-file/index.ts
@@ -1,16 +1,22 @@
-import * as fs from 'fs';
-import { Registry } from '../shared/model/registry';
+import * as fs from '@std/fs';
+import { Registry } from '../shared/model/registry.ts';
+import { checkArgs, parseArgs, printHelpText } from './cli.ts';
 
 
-const dataDir = process.argv[2];
-const outputDir = process.argv[3];
-if (!dataDir || !outputDir) {
-  console.error('Usage: node index.js <dataDir> <outputDir>');
-  process.exit(1);
+const args = parseArgs(Deno.args);
+
+if (args.help) {
+  printHelpText();
+  Deno.exit(0);
 }
 
+checkArgs(args);
 
-const electoralPeriodDirs = fs.readdirSync(dataDir).filter(f => fs.lstatSync(`${dataDir}/${f}`).isDirectory());
+
+const electoralPeriodDirs = Array
+  .from(Deno.readDirSync(args.dataDir))
+  .filter(f => Deno.statSync(`${args.dataDir}/${f.name}`).isDirectory)
+  .map(d => d.name);
 
 const routes: string[] = [];
 
@@ -18,13 +24,13 @@ routes.push('/'); // because of redirect to current electoral period
 
 for (const electoralPeriodDir of electoralPeriodDirs) {
 
-  const registryFilename = `${dataDir}/${electoralPeriodDir}/registry.json`;
-  if (!fs.existsSync(registryFilename) || !fs.lstatSync(registryFilename).isFile()) {
+  const registryFilename = `${args.dataDir}/${electoralPeriodDir}/registry.json`;
+  if (!fs.existsSync(registryFilename) || !Deno.statSync(registryFilename).isFile) {
     console.error(`Registry file "${registryFilename}" does not exist.`);
-    process.exit(1);
+    Deno.exit(1);
   }
 
-  const registry = JSON.parse(fs.readFileSync(registryFilename, 'utf-8')) as Registry;
+  const registry = JSON.parse(Deno.readTextFileSync(registryFilename)) as Registry;
 
   routes.push(`/ep/${registry.electoralPeriod}`);
   routes.push(`/ep/${registry.electoralPeriod}/parties`);
@@ -37,8 +43,8 @@ for (const electoralPeriodDir of electoralPeriodDirs) {
   routes.push(`/ep/${registry.electoralPeriod}/sessions`);
   routes.push(...registry.sessions.map(session => `/ep/${registry.electoralPeriod}/session/${session.id}`));
   for (const session of registry.sessions) {
-    const sessionScanFilename = `${dataDir}/${electoralPeriodDir}/${session.id}/session-scan-${session.id}.json`;
-    const sessionScan = JSON.parse(fs.readFileSync(sessionScanFilename, 'utf-8')) as any[];
+    const sessionScanFilename = `${args.dataDir}/${electoralPeriodDir}/${session.id}/session-scan-${session.id}.json`;
+    const sessionScan = JSON.parse(Deno.readTextFileSync(sessionScanFilename)) as Record<symbol, never>[];
     const votingIds = sessionScan.map((_, index: number) => index + 1);
     routes.push(...votingIds.map(votingId => `/ep/${registry.electoralPeriod}/session/${session.id}/voting/${votingId}`));
   }
@@ -69,6 +75,6 @@ routes.push('/ep/7/party/tierschutzallianz');
 routes.push('/ep/7/party/fdp');
 routes.push('/ep/7/party/parteilos-01');
 
-fs.writeFileSync(`${outputDir}/routes.txt`, routes.join('\n'));
+Deno.writeTextFileSync(`${args.outputDir}/routes.txt`, routes.join('\n'));
 
 console.log('Done.');
