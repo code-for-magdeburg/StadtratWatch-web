028863326701c411c28e506b7b5e45cdd135a352
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2024-11-29 19:06:02 +0100

    refactor generate-paper-assets

--- src/scripts/generate-paper-assets/index.ts
+++ src/scripts/generate-paper-assets/index.ts
@@ -1,8 +1,8 @@
-import * as fs from '@std/fs';
-import * as path from '@std/path';
-import { ScrapedFile, ScrapedMeeting, ScrapedSession } from '../shared/model/scraped-session.ts';
-import { PaperDto, PapersDto } from '@scope/interfaces-web-assets';
+import { ScrapedSession } from '../shared/model/scraped-session.ts';
 import { checkArgs, parseArgs, printHelpText } from './cli.ts';
+import { PaperFilesStore } from "./paper-files-store.ts";
+import { PaperAssetsGenerator } from './paper-assets-generator.ts';
+import { PaperAssetsStore } from './paper-assets-store.ts';
 
 
 const args = parseArgs(Deno.args);
@@ -15,82 +15,9 @@ if (args.help) {
 checkArgs(args);
 
 
+const paperFilesStore = new PaperFilesStore(args.papersDir);
+const paperAssetsStore = new PaperAssetsStore(args.outputDir);
+const generator = new PaperAssetsGenerator(paperFilesStore, paperAssetsStore);
 const scrapedSession = JSON.parse(Deno.readTextFileSync(args.scrapedSessionFilename)) as ScrapedSession;
-run(scrapedSession, args.outputDir);
-
+generator.generatePaperAssets(scrapedSession);
 console.log('Done.');
-
-
-function run(scrapedSession: ScrapedSession, outputDir: string) {
-
-  const allPapers: PapersDto = [];
-
-  scrapedSession.meetings
-    .filter(
-      meeting =>
-        !meeting.cancelled
-        && !!meeting.original_id
-        && meeting.organization_name === 'Stadtrat'
-      // && meeting.start >= '2019'
-    )
-    .forEach(meeting => {
-
-      scrapedSession.agenda_items
-        .filter(agendaItem => agendaItem.meeting_id === meeting.original_id)
-        .filter(agendaItem => !!agendaItem.paper_original_id)
-        .filter(
-          agendaItem => !allPapers.some(paper => paper.id === agendaItem.paper_original_id!)
-        )
-        .forEach(agendaItem => {
-
-          const paperId = agendaItem.paper_original_id!;
-          const files = scrapedSession.files
-            .filter(file => file.paper_original_id === paperId)
-            .map(file => ({
-              id: file.original_id,
-              name: file.name,
-              url: file.url,
-              size: getFileSize(meeting, file),
-            }));
-          const paper = scrapedSession.papers.find(p => p.original_id === paperId);
-          allPapers.push({
-            id: paperId,
-            reference: agendaItem.paper_reference,
-            type: paper ? paper.paper_type : null,
-            title: paper ? paper.name : '',
-            files,
-          });
-
-        });
-
-    });
-
-  const flattened = allPapers.flat().sort((a, b) => a.id - b.id);
-  console.log('Found papers: ', flattened.length);
-
-  // Group papers (flattened) in batches of 1000.
-  const grouped = flattened.reduce((acc, paper) => {
-    const batchNo = `${Math.floor(paper.id / 1000)}`.padStart(4, '0');
-    if (!acc[batchNo]) {
-      acc[batchNo] = [];
-    }
-    acc[batchNo].push(paper);
-    return acc;
-  }, {} as { [batchNo: string]: PaperDto[] });
-
-  for (const batchNo in grouped) {
-    const filename = path.join(outputDir, `papers-${batchNo}.json`);
-    Deno.writeTextFileSync(filename, JSON.stringify(grouped[batchNo], null, 2));
-  }
-
-}
-
-
-function getFileSize(meeting: ScrapedMeeting, file: ScrapedFile): number | null {
-
-  const year = meeting.start.split('-')[0];
-  const paperFilename = path.join(args.papersDir, year, `${file.original_id}.pdf`);
-
-  return fs.existsSync(paperFilename) ? Deno.statSync(paperFilename).size : null;
-
-}
---
+++ src/scripts/generate-paper-assets/paper-assets-generator.ts
@@ -0,0 +1,62 @@
+import { ScrapedSession } from '../shared/model/scraped-session.ts';
+import { IPaperAssetsStore } from './paper-assets-store.ts';
+import { IPaperFilesStore } from './paper-files-store.ts';
+import { PapersDto } from '@scope/interfaces-web-assets';
+
+
+export class PaperAssetsGenerator {
+
+
+  constructor(private readonly paperFilesStore: IPaperFilesStore, private readonly assetsStore: IPaperAssetsStore) {
+  }
+
+
+  public generatePaperAssets(scrapedSession: ScrapedSession) {
+
+    const allPapers: PapersDto = [];
+
+    scrapedSession.meetings
+      .filter(meeting => !meeting.cancelled)
+      .filter(meeting => !!meeting.original_id)
+      .filter(meeting => meeting.organization_name === 'Stadtrat')
+      // .filter(meeting => meeting.start >= '2019')
+      .forEach(meeting => {
+
+        scrapedSession.agenda_items
+          .filter(agendaItem => agendaItem.meeting_id === meeting.original_id)
+          .filter(agendaItem => !!agendaItem.paper_original_id)
+          .filter(
+            agendaItem => !allPapers.some(paper => paper.id === agendaItem.paper_original_id!)
+          )
+          .forEach(agendaItem => {
+
+            const paperId = agendaItem.paper_original_id!;
+            const files = scrapedSession.files
+              .filter(file => file.paper_original_id === paperId)
+              .map(file => ({
+                id: file.original_id,
+                name: file.name,
+                url: file.url,
+                size: this.paperFilesStore.getFileSize(meeting, file),
+              }));
+            const paper = scrapedSession.papers.find(p => p.original_id === paperId);
+            allPapers.push({
+              id: paperId,
+              reference: agendaItem.paper_reference,
+              type: paper ? paper.paper_type : null,
+              title: paper ? paper.name : '',
+              files,
+            });
+
+          });
+
+      });
+
+    console.log('Found papers: ', allPapers.length);
+
+    this.assetsStore.writePaperAssets(allPapers);
+
+  }
+
+
+}
