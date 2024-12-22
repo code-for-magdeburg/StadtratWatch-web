d4d840a2e5e42a2b1fd72670f9b4d91553ef88b9
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2024-12-22 17:43:07 +0100

    add font

--- astro/tailwind.config.mjs
+++ astro/tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        neutral: colors.neutral
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    },
+  },
+  plugins: [],
 }
