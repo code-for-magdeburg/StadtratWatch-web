f45c4fa2d49af1d459c2dc3459097bd4caebd538
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2024-12-31 21:58:35 +0100

    add alpinejs

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpinejs';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })]
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    alpinejs()
+  ]
 });
