3016a00b7d74320e600b93ba083e8693d1c399cf
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2025-01-06 20:35:08 +0100

    implement session page

---
+++ astro/src/model/session-config.ts
@@ -0,0 +1,36 @@
+export type SessionConfig = {
+  youtubeUrl: string;
+  layout: SessionConfigLayout;
+  names: SessionConfigPerson[];
+};
+
+export type SessionConfigLayout = {
+  namesRowHeight: number;
+  namesColumns: SessionConfigColumn[];
+  videoTimestampRectangle: SessionConfigLayoutRectangle;
+  votingSubjectIdRectangle: SessionConfigLayoutRectangle;
+  votingSubjectTitleRectangle: SessionConfigLayoutRectangle;
+};
+
+export type SessionConfigPerson = {
+  name: string;
+  columnIndex: number;
+  rowIndex: number;
+  party: string;
+  faction: string;
+};
+
+export type SessionConfigColumn = {
+  left: number;
+  top: number;
+  width: number;
+  testPixelXOffset: number;
+  testPixelYOffset: number;
+};
+
+export type SessionConfigLayoutRectangle = {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+};
