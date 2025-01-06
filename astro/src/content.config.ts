17510fc53fedf1ea848825a2ac9277e3c7f278f2
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2025-01-06 09:21:26 +0100

    implement sessions page

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -1,5 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+import type { RegistrySession } from './model/registry.ts';
 
 
 const electoralPeriods = defineCollection({
@@ -12,11 +13,7 @@ const electoralPeriods = defineCollection({
     id: z.string(),
     name: z.string(),
     lastUpdate: z.string(),
-    sessions: z.array(z.object({
-      id: z.string(),
-      date: z.string(),
-      meetingMinutesUrl: z.string()
-    })),
+    sessions: z.array(z.custom<RegistrySession>()),
     factions: z.array(z.object({
       id: z.string(),
       name: z.string(),
@@ -39,4 +36,51 @@ const electoralPeriods = defineCollection({
 });
 
 
-export const collections = { electoralPeriods };
+const sessionScans = defineCollection({
+  loader: glob({
+    pattern: '**/session-scan-*.json',
+    base: '../data',
+    generateId: options => {
+      const entryParts = options.entry.split('/');
+      return `${entryParts[0]}/${entryParts[1]}`;
+    }
+  }),
+  schema: z.array(z.object({
+    votingFilename: z.string(),
+    videoTimestamp: z.string(),
+    votingSubject: z.object({
+      agendaItem: z.string(),
+      applicationId: z.string(),
+      title: z.string(),
+      type: z.string(),
+      authors: z.array(z.string())
+    }),
+    votes: z.array(z.object({
+      name: z.string(),
+      vote: z.string()
+    }))
+  }))
+});
+
+
+const sessionSpeeches = defineCollection({
+  loader: glob({
+    pattern: '**/session-speeches-*.json',
+    base: '../data',
+    generateId: options => {
+      const entryParts = options.entry.split('/');
+      return `${entryParts[0]}/${entryParts[1]}`;
+    }
+  }),
+  schema: z.array(z.object({
+    speaker: z.string(),
+    start: z.number(),
+    duration: z.number(),
+    isChairPerson: z.boolean().optional(),
+    onBehalfOf: z.string().optional(),
+    transcription: z.string().nullable()
+  }))
+});
+
+
+export const collections = { electoralPeriods, sessionScans, sessionSpeeches };
