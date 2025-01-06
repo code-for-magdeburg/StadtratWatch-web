6c9c2d35a1001e3bc914a84759548111479c7b99
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2025-01-06 20:35:08 +0100

    implement session page

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -1,6 +1,9 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
-import type { RegistrySession } from './model/registry.ts';
+import type { Registry } from './model/registry.ts';
+import type { SessionScan } from './model/session-scan.ts';
+import type { SessionSpeech } from './model/session-speech.ts';
+import type { SessionConfig } from './model/session-config.ts';
 
 
 const electoralPeriods = defineCollection({
@@ -9,30 +12,7 @@ const electoralPeriods = defineCollection({
     base: '../data',
     generateId: options => options.data.id as string,
   }),
-  schema: z.object({
-    id: z.string(),
-    name: z.string(),
-    lastUpdate: z.string(),
-    sessions: z.array(z.custom<RegistrySession>()),
-    factions: z.array(z.object({
-      id: z.string(),
-      name: z.string(),
-      seats: z.number()
-    })),
-    parties: z.array(z.object({
-      id: z.string(),
-      name: z.string(),
-      seats: z.number()
-    })),
-    persons: z.array(z.object({
-      id: z.string(),
-      name: z.string(),
-      factionId: z.string(),
-      partyId: z.string(),
-      start: z.string().nullable(),
-      end: z.string().nullable()
-    }))
-  })
+  schema: z.custom<Registry>()
 });
 
 
@@ -45,21 +25,7 @@ const sessionScans = defineCollection({
       return `${entryParts[0]}/${entryParts[1]}`;
     }
   }),
-  schema: z.array(z.object({
-    votingFilename: z.string(),
-    videoTimestamp: z.string(),
-    votingSubject: z.object({
-      agendaItem: z.string(),
-      applicationId: z.string(),
-      title: z.string(),
-      type: z.string(),
-      authors: z.array(z.string())
-    }),
-    votes: z.array(z.object({
-      name: z.string(),
-      vote: z.string()
-    }))
-  }))
+  schema: z.custom<SessionScan>()
 });
 
 
@@ -72,15 +38,21 @@ const sessionSpeeches = defineCollection({
       return `${entryParts[0]}/${entryParts[1]}`;
     }
   }),
-  schema: z.array(z.object({
-    speaker: z.string(),
-    start: z.number(),
-    duration: z.number(),
-    isChairPerson: z.boolean().optional(),
-    onBehalfOf: z.string().optional(),
-    transcription: z.string().nullable()
-  }))
+  schema: z.array(z.custom<SessionSpeech>())
 });
 
 
-export const collections = { electoralPeriods, sessionScans, sessionSpeeches };
+const sessionConfigs = defineCollection({
+  loader: glob({
+    pattern: '**/config-*.json',
+    base: '../data',
+    generateId: options => {
+      const entryParts = options.entry.split('/');
+      return `${entryParts[0]}/${entryParts[1]}`;
+    }
+  }),
+  schema: z.custom<SessionConfig>()
+});
+
+
+export const collections = { electoralPeriods, sessionScans, sessionSpeeches, sessionConfigs };
