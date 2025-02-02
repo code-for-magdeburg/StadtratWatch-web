471f43be3f8f3f3a6805fd64a9ab86d73bf6872f
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2025-02-02 11:00:23 +0100

    refactor speaking time

--- astro/src/data-analysis/SpeakingTime.ts
+++ astro/src/data-analysis/SpeakingTime.ts
@@ -10,40 +10,36 @@ export class SpeakingTime {
 
 
   public forFaction(faction: RegistryFaction): number {
-
     return this.sessions
-      .flatMap(({ config, speeches }) => {
-        const factionMembers = config.names.filter(name => name.faction === faction.name).map(name => name.name);
-        return speeches
-          .filter(speech => !speech.isChairPerson)
-          .filter(speech => factionMembers.includes(speech.speaker))
-          .map(speech => Math.round(speech.duration / 10) * 10);
+      .flatMap(session => {
+        const persons = session.config.names.filter(name => name.faction === faction.name).map(name => name.name);
+        return this.calcSpeakingTimeForSession(persons, session);
       })
-      .reduce((a, b) => a + b, 0);
-
+      .reduce((acc, duration) => acc + duration, 0);
   }
 
 
   public forParty(party: RegistryParty): number {
-
     return this.sessions
-      .flatMap(({ config, speeches }) => {
-        const partyMembers = config.names.filter(name => name.party === party.name).map(name => name.name);
-        return speeches
-          .filter(speech => !speech.isChairPerson)
-          .filter(speech => partyMembers.includes(speech.speaker))
-          .map(speech => Math.round(speech.duration / 10) * 10);
+      .flatMap(session => {
+        const persons = session.config.names.filter(name => name.party === party.name).map(name => name.name);
+        return this.calcSpeakingTimeForSession(persons, session);
       })
-      .reduce((a, b) => a + b, 0);
-
+      .reduce((acc, duration) => acc + duration, 0);
   }
 
 
   public forPerson(person: RegistryPerson): number {
     return this.sessions
       .filter(session => session.config.names.some(name => name.name === person.name))
-      .flatMap(session => session.speeches)
-      .filter(speech => speech.speaker === person.name)
+      .flatMap(session => this.calcSpeakingTimeForSession([person.name], session))
+      .reduce((acc, duration) => acc + duration, 0);
+  }
+
+
+  private calcSpeakingTimeForSession(persons: string[], session: SessionInput): number {
+    return session.speeches
+      .filter(speech => persons.includes(speech.speaker))
       .filter(speech => !speech.isChairPerson)
       .map(speech => Math.round(speech.duration / 10) * 10)
       .reduce((acc, duration) => acc + duration, 0);
