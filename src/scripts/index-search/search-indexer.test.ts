955cf0d4865374f100c6b8c94816c22d5d1155d6
Jens Winter-Huebenthal <jens.winter-huebenthal@bridgefield.de>
2025-04-30 10:39:30 +0200

    add title to SessionConfig

--- src/scripts/index-search/search-indexer.test.ts
+++ src/scripts/index-search/search-indexer.test.ts
@@ -24,6 +24,7 @@ class SpeechesSourceStub implements ISpeechesSource {
   getSpeeches(): IndexableSpeech[] {
 
     const config: SessionConfig = {
+      title: 'Session Title',
       youtubeUrl: '',
       layout: {
         namesRowHeight: 0,
