8e5afd74050b24ba014c4b6c24beefefda51d850
Jens Winter-Huebenthal <jens.winter-huebenthal@bridgefield.de>
2025-07-14 15:32:45 +0200

    ♻️ refactor: update route prefix from /ep/ to /pp/

--- astro/src/pages/api/v1/_helpers.ts
+++
@@ -1,9 +0,0 @@
-export function generateMeta() {
-  return {
-    stadtratwatch_api: {
-      version: '1.0',
-      license: 'CC0 1.0',
-      license_url: 'https://creativecommons.org/publicdomain/zero/1.0/',
-    },
-  };
-}
