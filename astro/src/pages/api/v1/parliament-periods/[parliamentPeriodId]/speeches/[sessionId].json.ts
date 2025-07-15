7944323637458415394f12a9204734eae6f87ae2
Jens Winter-Huebenthal <jens.winter-huebenthal@bridgefield.de>
2025-07-15 21:15:37 +0200

    ✨ feat: add speeches API endpoint

---
+++ astro/src/pages/api/v1/parliament-periods/[parliamentPeriodId]/speeches/[sessionId].json.ts
@@ -0,0 +1,72 @@
+import { getStaticPaths as getStaticParliamentPeriodPaths } from '../../[parliamentPeriodId].json.ts';
+import { generateMeta } from '../../../_helpers.ts';
+import { getEntry } from 'astro:content';
+import type { Registry, RegistrySession } from '@models/registry.ts';
+
+export const getStaticPaths = async () => {
+  const paths = await getStaticParliamentPeriodPaths();
+  return paths.flatMap((path) =>
+    path.props.parliamentPeriod.sessions.map((session) => ({
+      params: {
+        parliamentPeriodId: path.params.parliamentPeriodId,
+        sessionId: session.id,
+      },
+      props: {
+        parliamentPeriod: path.props.parliamentPeriod,
+        session,
+      },
+    })),
+  );
+};
+
+export async function GET({
+  params,
+  props,
+}: {
+  params: { parliamentPeriodId: string; sessionId: string };
+  props: { parliamentPeriod: Registry; session: RegistrySession };
+}) {
+  const { parliamentPeriodId, sessionId } = params;
+
+  const sessionSpeechesEntry = await getEntry(
+    'sessionSpeeches',
+    `${parliamentPeriodId}/${sessionId}`,
+  );
+  const { data: sessionSpeeches } = sessionSpeechesEntry || {};
+  if (!sessionSpeeches) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Session speeches not found',
+    });
+  }
+
+  const { parliamentPeriod } = props;
+  const personsMap = new Map(
+    parliamentPeriod.persons.map((person) => [person.name, person]),
+  );
+  const data = sessionSpeeches
+    .filter((speech) => !speech.isChairPerson)
+    .filter((speech) => !!speech.transcription)
+    .map((speech) => {
+      const person = personsMap.get(speech.speaker);
+      return {
+        personId: person ? person.id : null,
+        speaker: person?.name || speech.speaker,
+        start: speech.start,
+        duration: speech.duration,
+        transcription: speech.transcription,
+      };
+    });
+
+  const response = {
+    meta: generateMeta(),
+    data,
+  };
+
+  return new Response(JSON.stringify(response), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
