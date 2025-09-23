55d28049a4d7ab42e4e8c362a4981bb8785a52cd
Jens Winter-Huebenthal <jens.winter-huebenthal@bridgefield.de>
2025-09-23 22:38:53 +0200

    ♻️ refactor: use enum for OParl object types

--- src/scripts/scrape-oparl/scrape-oparl.ts
+++ src/scripts/scrape-oparl/scrape-oparl.ts
@@ -2,6 +2,18 @@ import * as path from '@std/path';
 import { existsSync } from '@std/fs/exists';
 
 
+export enum OparlObjectType {
+  Organization,
+  Person,
+  Meeting,
+  Paper,
+  Membership,
+  Location,
+  AgendaItem,
+  Consultation,
+  File,
+}
+
 export type OparlSystem = {
   organization: string;
   person: string;
@@ -67,15 +79,15 @@ export class OparlScraper {
 
     const oparlSystem = await this.fetchOparlSystem(oparlSystemUrl);
 
-    await this.fetchAndStoreObjects(oparlSystem.organization, createdSince, 'organizations.json');
-    await this.fetchAndStoreObjects(oparlSystem.person, createdSince, 'persons.json');
-    await this.fetchAndStoreObjects(oparlSystem.meeting, createdSince, 'meetings.json');
-    await this.fetchAndStoreObjects(oparlSystem.paper, createdSince, 'papers.json');
-    await this.fetchAndStoreObjects(oparlSystem.membership, createdSince, 'memberships.json');
-    await this.fetchAndStoreObjects(oparlSystem.locationList, createdSince, 'locations.json');
-    await this.fetchAndStoreObjects(oparlSystem.agendaItem, createdSince, 'agenda-items.json');
-    await this.fetchAndStoreObjects(oparlSystem.consultations, createdSince, 'consultations.json');
-    await this.fetchAndStoreObjects(oparlSystem.files, createdSince, 'files.json');
+    await this.fetchAndStoreObjects(oparlSystem.organization, createdSince, OparlObjectType.Organization);
+    await this.fetchAndStoreObjects(oparlSystem.person, createdSince, OparlObjectType.Person);
+    await this.fetchAndStoreObjects(oparlSystem.meeting, createdSince, OparlObjectType.Meeting);
+    await this.fetchAndStoreObjects(oparlSystem.paper, createdSince, OparlObjectType.Paper);
+    await this.fetchAndStoreObjects(oparlSystem.membership, createdSince, OparlObjectType.Membership);
+    await this.fetchAndStoreObjects(oparlSystem.locationList, createdSince, OparlObjectType.Location);
+    await this.fetchAndStoreObjects(oparlSystem.agendaItem, createdSince, OparlObjectType.AgendaItem);
+    await this.fetchAndStoreObjects(oparlSystem.consultations, createdSince, OparlObjectType.Consultation);
+    await this.fetchAndStoreObjects(oparlSystem.files, createdSince, OparlObjectType.File);
 
   }
 
@@ -86,15 +98,15 @@ export class OparlScraper {
 
     const oparlSystem = await this.fetchOparlSystem(oparlSystemUrl);
 
-    await this.fetchAndStoreIncrementalObjects(oparlSystem.organization, modifiedSince, 'organizations.json');
-    await this.fetchAndStoreIncrementalObjects(oparlSystem.person, modifiedSince, 'persons.json');
-    await this.fetchAndStoreIncrementalObjects(oparlSystem.meeting, modifiedSince, 'meetings.json');
-    await this.fetchAndStoreIncrementalObjects(oparlSystem.paper, modifiedSince, 'papers.json');
-    await this.fetchAndStoreIncrementalObjects(oparlSystem.membership, modifiedSince, 'memberships.json');
-    await this.fetchAndStoreIncrementalObjects(oparlSystem.locationList, modifiedSince, 'locations.json');
-    await this.fetchAndStoreIncrementalObjects(oparlSystem.agendaItem, modifiedSince, 'agenda-items.json');
-    await this.fetchAndStoreIncrementalObjects(oparlSystem.consultations, modifiedSince, 'consultations.json');
-    await this.fetchAndStoreIncrementalObjects(oparlSystem.files, modifiedSince, 'files.json');
+    await this.fetchAndStoreIncrementalObjects(oparlSystem.organization, modifiedSince, OparlObjectType.Organization);
+    await this.fetchAndStoreIncrementalObjects(oparlSystem.person, modifiedSince, OparlObjectType.Person);
+    await this.fetchAndStoreIncrementalObjects(oparlSystem.meeting, modifiedSince, OparlObjectType.Meeting);
+    await this.fetchAndStoreIncrementalObjects(oparlSystem.paper, modifiedSince, OparlObjectType.Paper);
+    await this.fetchAndStoreIncrementalObjects(oparlSystem.membership, modifiedSince, OparlObjectType.Membership);
+    await this.fetchAndStoreIncrementalObjects(oparlSystem.locationList, modifiedSince, OparlObjectType.Location);
+    await this.fetchAndStoreIncrementalObjects(oparlSystem.agendaItem, modifiedSince, OparlObjectType.AgendaItem);
+    await this.fetchAndStoreIncrementalObjects(oparlSystem.consultations, modifiedSince, OparlObjectType.Consultation);
+    await this.fetchAndStoreIncrementalObjects(oparlSystem.files, modifiedSince, OparlObjectType.File);
 
   }
 
@@ -108,14 +120,15 @@ export class OparlScraper {
   }
 
 
-  private async fetchAndStoreObjects(objectsUrl: string, createdSince: string | null, filename: string): Promise<void> {
+  private async fetchAndStoreObjects(objectsUrl: string, createdSince: string | null, objectType: OparlObjectType): Promise<void> {
     const objects = await this.client.fetchObjects(objectsUrl, createdSince);
+    const filename = this.getObjectTypeFilename(objectType);
     this.oparlObjectsStore.saveObjects(objects, filename);
   }
 
 
   private async fetchAndStoreIncrementalObjects(objectsUrl: string, modifiedSince: string,
-                                                filename: string): Promise<void> {
+                                                objectType: OparlObjectType): Promise<void> {
 
     const modifiedObjects = await this.client.fetchModifiedObjects(objectsUrl, modifiedSince);
     if (modifiedObjects.length === 0) {
@@ -123,6 +136,7 @@ export class OparlScraper {
       return;
     }
 
+    const filename = this.getObjectTypeFilename(objectType);
     const existingObjects = this.oparlObjectsStore.loadObjects(filename);
     const updatedObjects = this.updateObjects(existingObjects, modifiedObjects);
     this.oparlObjectsStore.saveObjects(updatedObjects, filename);
@@ -145,6 +159,32 @@ export class OparlScraper {
 
     return updatedObjects;
 
+  }
+
+
+  private getObjectTypeFilename(objectType: OparlObjectType): string {
+    switch (objectType) {
+      case OparlObjectType.Organization:
+        return 'organizations.json';
+      case OparlObjectType.Person:
+        return 'persons.json';
+      case OparlObjectType.Meeting:
+        return 'meetings.json';
+      case OparlObjectType.Paper:
+        return 'papers.json';
+      case OparlObjectType.Membership:
+        return 'memberships.json';
+      case OparlObjectType.Location:
+        return 'locations.json';
+      case OparlObjectType.AgendaItem:
+        return 'agenda-items.json';
+      case OparlObjectType.Consultation:
+        return 'consultations.json';
+      case OparlObjectType.File:
+        return 'files.json';
+      default:
+        throw new Error('Unknown OParl object type');
+    }
   }
 
 
